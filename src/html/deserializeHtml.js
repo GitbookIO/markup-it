68967be45a74111e49683268e78edbbee3121b55
Samy Pessé <samypesse@gmail.com>
2016-12-07 14:43:39 +0100

    Add basic wrapping for blocks

--- src/html/deserializeHtml.js
+++ src/html/deserializeHtml.js
@@ -1,8 +1,9 @@
 const htmlparser = require('htmlparser2');
 const { List, Stack, Set } = require('immutable');
+const { Document } = require('slate');
 const { Deserializer } = require('../');
 const {
-    BLOCKS, INLINES, MARKS,
+    BLOCKS, INLINES, MARKS, CONTAINERS,
     Block, Inline, Text, Mark
 } = require('../');
 
@@ -90,7 +91,9 @@ function resolveHeadingAttrs(attribs) {
  */
 function parse(str) {
     // For convenience, starts with a root node
-    const root = new Block();
+    const root = Document.create({
+        type: BLOCKS.DOCUMENT
+    });
     // The top of the stack always hold the current parent
     // node. Should never be empty.
     let stack = Stack().push(root);
@@ -100,8 +103,24 @@ function parse(str) {
     // Append a node child to the current parent node
     function appendNode(node) {
         const parent = stack.peek();
+        const containerChildTypes = CONTAINERS[parent.type];
+
+        // Wrap node if type is not allowed
+        if (
+            containerChildTypes
+            && (node.kind !== 'block' || !containerChildTypes.includes(node.type))
+        ) {
+            console.log('wrap', node.type, 'for', parent.type);
+            node = Block.create({
+                type: containerChildTypes[0],
+                nodes: [node]
+            });
+        }
+
         const nodes = parent.nodes.push(node);
-        stack = stack.pop().push(parent.merge({ nodes }));
+        stack = stack
+            .pop()
+            .push(parent.merge({ nodes }));
     }
 
     // Push a new node, as current parent. We started parsing it
