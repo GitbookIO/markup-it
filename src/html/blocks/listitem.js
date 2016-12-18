8f60da47fb14bee0d8b1494be018d98ff1d92bdf
Samy Pesse <samypesse@gmail.com>
2016-12-18 14:09:57 +0100

    Serialize task lists to HTML

--- src/html/blocks/listitem.js
+++ src/html/blocks/listitem.js
@@ -8,11 +8,20 @@ const serialize = Serializer()
     .matchType(BLOCKS.LIST_ITEM)
     .then(state => {
         const node = state.peek();
-        const inner = state.serialize(node.nodes);
+        let inner = state.serialize(node.nodes);
+        let tag = '<li>';
+
+        const isTaskList = node.data.has('checked');
+        const isChecked = node.data.get('checked');
+
+        if (isTaskList) {
+            tag = '<li class="task-list-item">';
+            inner = `<input type="checkbox" class="task-list-item-checkbox"${isChecked ? ' checked' : ''} disabled /> ${inner}`;
+        }
 
         return state
             .shift()
-            .write(`<li>${inner}</li>\n`);
+            .write(`${tag}${inner}</li>\n`);
     });
 
 module.exports = { serialize };
