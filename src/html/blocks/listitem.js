7ff55f0e2c7bbed42e3d25120d1fff5aef8ffa98
Samy Pessé <samypesse@gmail.com>
2016-12-19 10:21:56 +0100

    Use className instead of tag

--- src/html/blocks/listitem.js
+++ src/html/blocks/listitem.js
@@ -9,19 +9,19 @@ const serialize = Serializer()
     .then(state => {
         const node = state.peek();
         let inner = state.serialize(node.nodes);
-        let tag = '<li>';
+        let className = '';
 
         const isTaskList = node.data.has('checked');
         const isChecked = node.data.get('checked');
 
         if (isTaskList) {
-            tag = '<li class="task-list-item">';
+            className = ' class="task-list-item"';
             inner = `<input type="checkbox" class="task-list-item-checkbox"${isChecked ? ' checked' : ''} disabled /> ${inner}`;
         }
 
         return state
             .shift()
-            .write(`${tag}${inner}</li>\n`);
+            .write(`<li${className}>${inner}</li>\n`);
     });
 
 module.exports = { serialize };
