a89e6997c7b04e31b66dd457f5556542c0a8032b
Samy Pesse <samypesse@gmail.com>
2016-12-23 22:33:26 +0100

    Add serialization for paragraphs and hr

--- src/asciidoc/index.js
+++ src/asciidoc/index.js
@@ -1,11 +1,15 @@
 const deserialize = require('./deserialize');
 const heading = require('./blocks/heading');
+const paragraph = require('./blocks/paragraph');
+const hr = require('./blocks/hr');
 const text = require('./inlines/text');
 
 module.exports = {
     block: [
         { deserialize },
-        heading
+        heading,
+        paragraph,
+        hr
     ],
     inline: [
         text
