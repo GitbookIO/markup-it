67cd755eadf9f08f1a921431006ef86218a123cb
Soreine <soreine.plume@gmail.com>
2018-03-28 14:39:29 +0200

    Prettier

--- src/markdown/re/block.js
+++ src/markdown/re/block.js
@@ -40,17 +40,39 @@ const _tag = '(?!(?:'
     + '|var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo'
     + '|span|br|wbr|ins|del|img)\\b)\\w+(?!:\\/|[^\\w\\s@]*@)\\b';
 
-block.list.item = replace(block.list.item, 'gm')(/allbull/g, block.list.bullet)(/bullet/g, block.list.bullet)();
+block.list.item = replace(block.list.item, 'gm')
+    (/allbull/g, block.list.bullet)
+    (/bullet/g, block.list.bullet)
+    ();
 
-block.blockquote = replace(block.blockquote)('def', block.def)();
+block.blockquote = replace(block.blockquote)
+    ('def', block.def)
+    ();
 
-block.list.block = replace(block.list.block)(/allbull/g, block.list.bullet)('hr', '\\n+(?=\\1?(?:[-*_] *){3,}(?:\\n+|$))')('def', '\\n+(?=' + block.def.source + ')')('footnote', block.footnote)();
+block.list.block = replace(block.list.block)
+    (/allbull/g, block.list.bullet)
+    ('hr', '\\n+(?=\\1?(?:[-*_] *){3,}(?:\\n+|$))')
+    ('def', '\\n+(?=' + block.def.source + ')')
+    ('footnote', block.footnote)
+    ();
 
-block.list.block_ul = replace(block.list.block)(/bullet/g, block.list.bullet_ul)();
-block.list.block_ol = replace(block.list.block)(/bullet/g, block.list.bullet_ol)();
-block.list.block = replace(block.list.block)(/bullet/g, block.list.bullet)();
+block.list.block_ul = replace(block.list.block)
+    (/bullet/g, block.list.bullet_ul)
+    ();
 
-block.html = replace(block.html)('comment', /<!--[\s\S]*?-->/)('closed', /<(tag)[\s\S]+?<\/\1>/)('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)(/tag/g, _tag)();
+block.list.block_ol = replace(block.list.block)
+    (/bullet/g, block.list.bullet_ol)
+    ();
+block.list.block = replace(block.list.block)
+    (/bullet/g, block.list.bullet)
+    ();
+
+block.html = replace(block.html)
+    ('comment', /<!--[\s\S]*?-->/)
+    ('closed', /<(tag)[\s\S]+?<\/\1>/)
+    ('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)
+    (/tag/g, _tag)
+    ();
 
 block.paragraph = replace(block.paragraph)
     ('notParagraphPart', notParagraphPart)
@@ -71,9 +93,13 @@ block.paragraph = replace(block.paragraph)
     ('tablenp', table.nptable)
     ();
 
-block.paragraph = replace(block.paragraph)('(?!', '(?!'
+block.paragraph = replace(block.paragraph)
+    ('(?!',
+        '(?!'
         + block.fences.source.replace('\\1', '\\2') + '|'
-        + block.list.block_ol.source.replace('\\1', '\\3') + '|'
-    )();
+        + block.list.block_ol.source.replace('\\1', '\\3')
+        + '|'
+    )
+    ();
 
 module.exports = block;
