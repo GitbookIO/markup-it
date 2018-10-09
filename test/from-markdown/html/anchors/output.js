87dfdec0fbe5155f8bd4ffa46ec0e908daef1a10
Soreine <soreine.plume@gmail.com>
2018-10-09 17:37:13 +0200

    Add failing tests

---
+++ test/from-markdown/html/anchors/output.js
@@ -0,0 +1,12 @@
+/** @jsx h */
+import h from 'h';
+
+export default (
+    <document>
+        <paragraph>
+            This is a simple hack to add anchors to Markdown headings
+        </paragraph>
+        <heading_one id="custom-anchor">A heading</heading_one>
+        <paragraph>Hey</paragraph>
+    </document>
+);
