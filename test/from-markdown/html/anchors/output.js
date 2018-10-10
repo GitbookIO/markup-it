8f03d810e2b566ae2db2c9b8d53959250332aa0f
Soreine <soreine.plume@gmail.com>
2018-10-10 10:18:20 +0200

    Typos

--- test/from-markdown/html/anchors/output.js
+++ test/from-markdown/html/anchors/output.js
@@ -6,7 +6,7 @@ export default (
         <paragraph>
             This is a simple hack to add anchors to Markdown headings
         </paragraph>
-        <heading_one id="custom-anchor">A heading</heading_one>
+        <header_one id="custom-anchor">A heading</header_one>
         <paragraph>Hey</paragraph>
     </document>
 );
