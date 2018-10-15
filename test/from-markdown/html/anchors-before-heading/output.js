ac0849908ae987aee3be50deb047805ef3252130
Soreine <soreine.plume@gmail.com>
2018-10-15 08:56:26 +0200

    Fix tests

--- test/from-markdown/html/anchors-before-heading/output.js
+++ test/from-markdown/html/anchors-before-heading/output.js
@@ -6,6 +6,9 @@ export default (
         <paragraph>
             This is a simple hack to add anchors to Markdown headings
         </paragraph>
+        <paragraph>
+            <text />
+        </paragraph>
         <header_one id="custom-anchor">A heading</header_one>
         <paragraph>Hey</paragraph>
     </document>
