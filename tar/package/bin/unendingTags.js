da566e556007d3a9cd7e4759d140f9ac457324d6
Johan Preynat <johan.preynat@gmail.com>
2019-11-25 18:18:11 +0100

    Remove test tar

--- tar/package/bin/unendingTags.js
+++
@@ -1,6 +0,0 @@
-// Unending tags that can be used in the tests
-// Also used by the bin/ scripts for console printing
-const unendingTags = ['sample', 'common'];
-module.exports = {
-    unendingTags
-};
